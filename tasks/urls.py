c128db6367d588985fc3eb489bafa484df28cc0e
codexprime <samailaperey2@gmail.com>
2025-11-23 14:28:33 -1200

    Updated with login page

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('update_task/<str:pk>', views.updateTask, name='update'),
     path('delete/<str:pk>', views.deleteTask, name='delete'),
     path('register/', views.registerUser, name='register'),    
+    path('login/', views.loginUser, name='login'),
 ]
 
