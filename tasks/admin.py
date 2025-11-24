9620782583365f0f275f8629f0f11c490bc03d77
codexprime <samailaperey2@gmail.com>
2025-11-24 09:34:03 -1200

    Added category model with manay to many rerlationships

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
+from .models import Task, Category
 # Register your models here.
 
 from .models import *
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color']
+    list_editable = ['color']
+    
 admin.site.register(Task) 
+admin.site.register(Category, CategoryAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,14 +1,25 @@
-from turtle import mode
+from turtle import color, mode
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    color = models.CharField(max_length=7, default='#00f5ff')
+    
+    class Meta:
+        verbose_name_plural = 'Categories' #correct plural in admin
+        
+    def __str__(self):
+        return self.name
+    
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, blank=True)
         
      
     def __str__(self):
